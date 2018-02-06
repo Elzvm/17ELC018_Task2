d10a5b8821d70b70ab6fdeb4d5afb4dd2bb4c0d7
Zahckoh Mitchell dos Santos <32768945+Elzvm@users.noreply.github.com>
2018-02-06 15:44:40 +0000

    Update Motor.h

--- Motor.h
+++ Motor.h
@@ -1,1 +1,11 @@
-
+class motor{
+  public:	motor(unsigned char);
+  void start motor();
+  void stop motor();
+  void program position();
+  private:
+  unsigned char port_map;
+  int motor_speed;
+  bool motor_direction;
+  bool motor status;
+};
